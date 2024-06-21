b66e7189ebdb526f54b20ff0b18ff9c87397b33d
marimo <99727726+marimarimo0228@users.noreply.github.com>
2024-06-21 15:13:10 +0900

    t

--- 10/ex10.js
+++ 10/ex10.js
@@ -19,3 +19,10 @@ function kadai4(){
     kara.push('hoge','fuga','piyo');
     alert(kara);
 }
+
+function kadai6(){
+    let like =['あんぱん','食パン','カレーパン','メロンパン','バター']
+    for(n = 1;n <= 5;n = n + 1){
+        
+    }
+}
