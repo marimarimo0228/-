13df0593cb60b6f30b50f7892013b26f4e3e4595
marimo <99727726+marimarimo0228@users.noreply.github.com>
2024-06-24 16:57:57 +0900

    t

--- 10/ex10.js
+++ 10/ex10.js
@@ -1,25 +1,3 @@
-let fruit =['apple','strawberry','banana','melon','qui']
-
-function kadai1(){
-    alert(fruit);
-}
-
-function kadai2(){
-    fruit.push('peach');
-    alert(fruit);
-}
-
-function kadai3(){
-    alert(fruit[1]);
-}
-
-let kara =[];
-
-function kadai4(){
-    kara.push('hoge','fuga','piyo');
-    alert(kara);
-}
-
 function kadai6(){
     let like =['あんぱん','食パン','カレーパン','メロンパン','バター']
     for(n = 1;n <= 5;n = n + 1){
